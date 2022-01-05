56b097c610544d84b6663a0dea3bf886b82053b6
Mehmet Aziz Algüllü <81918844+maziz-create@users.noreply.github.com>
2022-01-05 17:48:13 +0000

    oyuncuEkle func

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 struct oyuncu  
 {  
   char isim[25];
-  char soyIsim[20];
+  char soyIsim[25];
+  int hataliPas;
   int oynananMacSayisi;  
-  int hataliPas;  
-  int isabetliPas;  
+  int isabetliPas;
 };
 
 bool oyuncununKaydiVarMi(struct oyuncu gelenOyuncu, char gelenOyuncuAdi[20]) {
@@ -19,41 +20,55 @@ bool oyuncununKaydiVarMi(struct oyuncu gelenOyuncu, char gelenOyuncuAdi[20]) {
   return false;
 }
 
-void oyuncuEkle(struct oyuncu gelenOyuncu) {
-
+void oyuncuEkle(struct oyuncu oyuncular[20], struct oyuncu eklenecekOyuncu, int kacinciOyuncu) {
+  printf("gelen isim => %s \n", eklenecekOyuncu.isim);
+  printf("gelen soyisim => %s \n", eklenecekOyuncu.soyIsim);
+  printf("gelen hatali pas => %d \n", eklenecekOyuncu.hataliPas);
+  printf("gelen isabetli pas => %d \n", eklenecekOyuncu.isabetliPas);
 }
-
 
 int main()
 {
   struct oyuncu oyuncular[20];
-  strcpy( oyuncular[0].isim, "Can");
-  strcpy( oyuncular[1].isim, "Deniz");
-  strcpy( oyuncular[2].isim, "Umit");
+  struct oyuncu temp;
 
   char satir[100], *karakter;
-  char neyeGore[] = " ";
+  char neyeGore[] = " ", *ptr;
   FILE *paslar = fopen("paslar.txt", "r");
-  char *ptr;
 
+  bool kaydiVarMi = false;
   int kacinciOyuncu = 0;
+  int ozellikSayaci = 0;
  
   do {
     karakter = fgets(satir, 100, paslar);
     ptr = strtok(satir, neyeGore);
-    if (oyuncununKaydiVarMi(oyuncular[kacinciOyuncu], ptr)) {
-      printf("Kaydı var!\n");
-    } else {
-      printf("Kaydı yok!\n");
-    }
+
+    kaydiVarMi = oyuncununKaydiVarMi(oyuncular[kacinciOyuncu], ptr);
+
     while(ptr != NULL)
 	  {
-		  printf("%s\n", ptr);
+      if (!kaydiVarMi) {
+        if (ozellikSayaci == 0) {
+          strcpy(temp.isim , ptr);
+        }else if (ozellikSayaci == 1){
+          strcpy(temp.soyIsim , ptr);
+        }else if (ozellikSayaci == 2){
+          temp.hataliPas = atoi(ptr);
+        }else{
+          temp.isabetliPas = atoi(ptr);
+        }
+      }
 		  ptr = strtok(NULL, neyeGore);
+      ozellikSayaci++;
 	  }
+
+    oyuncuEkle(oyuncular, temp, kacinciOyuncu);
+    printf("\n\n\n");
+
+    ozellikSayaci = 0;
     kacinciOyuncu++;
   } while (karakter != NULL);
-
 
 	return 0;
 }
